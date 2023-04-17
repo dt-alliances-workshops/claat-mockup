6f2815435edf8b3e9ba5996c8734deaac68b12e0
Michael Villiger <akirasoft@gmail.com>
2023-04-17 15:40:13 -0500

    changing to mount views

--- docker-build-and-view.sh
+++ docker-build-and-view.sh
@@ -1,3 +1,3 @@
-docker run -it -v ${PWD}/workshop-markdown:/usr/src/app/workshop-markdown -v ${PWD}/dist:/usr/src/app/dist-final mvilliger/workshop-builder:0.3 &&
+docker run -it -v ${PWD}/workshop-markdown:/usr/src/app/workshop-markdown -v ${PWD}/dist:/usr/src/app/dist-final -v ${PWD}/app/views:/usr/src/app/app/views mvilliger/workshop-builder:0.3 &&
 ./test.sh && 
 docker run  -it -v ${PWD}/dist:/usr/share/nginx/html:ro -p 8009:80 nginx
